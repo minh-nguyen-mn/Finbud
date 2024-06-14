52a62564ec7842f9b529f76e18d53197a6d121bf
KhoiTran0310 <kat46@njit.edu>
2024-06-14 15:05:22 +0700

    add riskchat

--- backend/functions/analyzeRisk.js
+++ backend/functions/analyzeRisk.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 require('dotenv').config();
 const moment = require('moment');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-// const genAI = new GoogleGenerativeAI('process.env.API_KEY');
-const genAI = new GoogleGenerativeAI('AIzaSyBoqZUePAhe5n5INyoApGlytjx57t8-UYI');
+const genAI = new GoogleGenerativeAI('process.env.API_KEY');
+// const genAI = new GoogleGenerativeAI('AIzaSyBoqZUePAhe5n5INyoApGlytjx57t8-UYI');
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 exports.handler = async (req) =>{
     try{
